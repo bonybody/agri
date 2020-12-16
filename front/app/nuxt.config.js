25dd41ee78acb334773d63901f4c80d6de1bbab8
bonybody <yumaaa312@gmail.com>
2020-12-16 11:34:12 +0900

    環境最適化

--- front/app/nuxt.config.js
+++ front/app/nuxt.config.js
@@ -18,7 +18,6 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '@plugins/register_base_components.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -26,14 +25,13 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
@@ -41,5 +39,11 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    hardSource: true
+  },
+  watchers: {
+    webpack: {
+      poll: true
+    }
   }
 }
