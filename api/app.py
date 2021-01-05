eb2e1593821b1f33154fbb490e5a3b92d0e7ded8
bonybody <yumaaa312@gmail.com>
2021-01-05 17:51:08 +0900

    blueprintでcontrollerを分割

--- api/app.py
+++ api/app.py
@@ -4,60 +4,27 @@
 import sys
 import codecs
 sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
-from flask_jwt import JWT, jwt_required, current_identity
-from flask_cors import CORS
-from werkzeug.security import safe_str_cmp
-from controller import test
+
+# アプリケーションの初期化
+from flask import Flask
 app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
+
+# 設定ファイルの読み込み
+app.config.from_pyfile("config/base_setting.py")
+
+# CORSを適応
+from flask_cors import CORS
 CORS(app)
-api = Api(app)
-class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
 
-    def __str__(self):
-        return "User(id='%s')" % self.id
+# JWT認証を追加
+from plugin.auth import *
 
-users = [
-    User(1, 'user1', 'user1'),
-    User(2, 'user2', 'abcxyz'),
-]
-
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
-
-def authenticate(username, password):
-    user = username_table.get(username, None)
-    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
-        return user
-
-def identity(payload):
-    user_id = payload['identity']
-    return userid_table.get(user_id, None)
-
-app.config['SECRET_KEY'] = 'super-secret'
-JWT_AUTH_HEADER_PREFIX='Bearer'
-jwt = JWT(app, authenticate, identity)
-
-class HelloWorld(Resource):
-  def get(self):
-    return {'hello': 'world'}
-
-@app.route('/protected')
-@jwt_required()
-def protected():
-    return user1
-
-
-api.add_resource(HelloWorld, '/')
-
-app.register_blueprint(test.app)
-
+# 各コントローラーをルーティングに追加
+import routing
+routing.register_controller_blueprint(app)
 
 if __name__ == '__main__':
   app.run()
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy
---
+++ api/plugin/auth.py
@@ -0,0 +1,31 @@
+from app import app
+from flask_jwt import JWT, jwt_required, current_identity
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+users = [
+    User(1, 'user1', 'user1'),
+    User(2, 'user2', 'abcxyz'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+JWT_AUTH_HEADER_PREFIX='Bearer'
+jwt = JWT(app, authenticate, identity)
