cdec83accc9b34a3140660eb9a48834ca55c2c0d
bonybody <yumaaa312@gmail.com>
2021-01-06 21:45:58 +0900

    グローバルなcssの追加

---
+++ front/components/atoms/buttons/AppButtonHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AppHeaderButton"
+}
+</script>
+
+<style scoped lang="scss">
+button {
+  display: inline-block;
+  height: 40px;
+  border: 1px solid $base-font-color;
+  border-radius: 10px;
+}
+</style>
---
+++ front/components/organisms/common/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TheHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
