b47b193e6d4d9b988aec2c89b16f5a2b8fc0c2f4
bonybody <yumaaa312@gmail.com>
2021-01-25 12:10:14 +0900

    ログインフォームの作成

---
+++ front/components/atoms/forms/AppFormButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="form-button" type="submit" @click="myClick()">
+      <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+  name: "AppFormButton",
+  methods: {
+    myClick() {
+      this.$emit('my-click')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.form-button {
+  text-align: center;
+  height: 40px;
+  border-radius: 4px;
+  width: 100%;
+  background-color: $primary-color;
+  color: $primary-on-font-color;
+}
+</style>
