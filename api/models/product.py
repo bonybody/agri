e18da66775e4178df512b9ce451f97aca0e12854
saki2710 <68329381+saki2710@users.noreply.github.com>
2021-02-19 12:54:28 +0900

    Update product.py

--- api/models/product.py
+++ api/models/product.py
@@ -10,27 +10,24 @@ class Product(db.Model):
     period = db.Column(db.Integer(2), nullable=False)
     quantity = db.Column(db.Integer(5), nullable=False)
     category = db.Column(db.String(255))
-    tag = db.Column(db.String(255))
     area = db.Column(db.String(255))
     price = db.Column(db.Integer(10), nullable=False)
-    exhibit = db.Column(db.DateTime, nullable=False, default=datetime.now)
     image = db.Column(db.String(255))
     state = db.Column(db.String(10))
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
-    def __init__(self, id, name='', contents='', period='', quantity='', category='', tag='', area='', price='', exhibit='', image='', state=''):
+    def __init__(self, id, name='', contents='', period='', quantity='', category='', area='', price='', image='', state='', created_at=''):
         self.name = name
         self.contents = contents
         self.period = period
         self.quantity = quantity
         self.category = category
-        self.tag = tag
         self.area = area
         self.price = price
-        self.exhibit = exhibit
         self.image = image
         self.state = state
+        self.created_at = created_at
 
     @classmethod
     def getUserInfo(cls, user_id):
