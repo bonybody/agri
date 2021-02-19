e2f0689b771b44f3b4b1afe4d6c38b06eba691fa
saki2710 <68329381+saki2710@users.noreply.github.com>
2021-02-19 12:03:08 +0900

    Update product.py

--- api/models/product.py
+++ api/models/product.py
@@ -4,33 +4,33 @@ from api.database.database import db
 
 class Product(db.Model):
     __tablename__ = 't_product'
-    p_code = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    p_name = db.Column(db.String(20), nullable=False)
-    p_contents = db.Column(db.String(255))
-    p_period = db.Column(db.Integer(2), nullable=False)
-    p_quantity = db.Column(db.Integer(5), nullable=False)
-    p_category = db.Column(db.String(255))
-    p_tag = db.Column(db.String(255))
-    p_area = db.Column(db.String(255))
-    p_price = db.Column(db.Integer(10), nullable=False)
-    p_exhibit = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    p_image = db.Column(db.String(255))
-    p_state = db.Column(db.String(10))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False)
+    contents = db.Column(db.String(255))
+    period = db.Column(db.Integer(2), nullable=False)
+    quantity = db.Column(db.Integer(5), nullable=False)
+    category = db.Column(db.String(255))
+    tag = db.Column(db.String(255))
+    area = db.Column(db.String(255))
+    price = db.Column(db.Integer(10), nullable=False)
+    exhibit = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    image = db.Column(db.String(255))
+    state = db.Column(db.String(10))
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
-    def __init__(self, p_code, p_name='', p_contents='', p_period='', p_quantity='', p_category='', p_tag='', p_area='', p_price='', p_exhibit='', p_image='', p_state=''):
-        self.p_name = p_name
-        self.p_contents = p_contents
-        self.p_period = p_period
-        self.p_quantity = p_quantity
-        self.p_category = p_category
-        self.p_tag = p_tag
-        self.p_area = p_area
-        self.p_price = p_price
-        self.p_exhibit = p_exhibit
-        self.p_image = p_image
-        self.p_state = p_state
+    def __init__(self, id, name='', contents='', period='', quantity='', category='', tag='', area='', price='', exhibit='', image='', state=''):
+        self.name = name
+        self.contents = contents
+        self.period = period
+        self.quantity = quantity
+        self.category = category
+        self.tag = tag
+        self.area = area
+        self.price = price
+        self.exhibit = exhibit
+        self.image = image
+        self.state = state
 
     @classmethod
     def getUserInfo(cls, user_id):
