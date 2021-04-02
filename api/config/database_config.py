46ed762296d3f0fe8694d932871cf0090dec67a1
bonybody <yumaaa312@gmail.com>
2021-04-03 04:54:13 +0900

    DB変更(mysql->postgres)

--- api/config/database_config.py
+++ api/config/database_config.py
@@ -1,8 +1,8 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
-    'user': os.environ['MYSQL_USER'],
-    'password': os.environ['MYSQL_PASSWORD'],
+SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
+    'user': os.environ['POSTGRES_USER'],
+    'password': os.environ['POSTGRES_PASSWORD'],
     'host': os.environ['DB_HOST'],
-    'db_name': os.environ['MYSQL_DATABASE']
+    'db_name': os.environ['POSTGRES_DATABASE']
 })
